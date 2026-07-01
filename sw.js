7901885d38663a9fdfbe271b26c7f199ab12d17f
VoltaReborn <thecollinsk@gmail.com>
2026-07-01 15:59:44 -0500

    version stuff

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'golf-pwa-v47';
+const CACHE_NAME = 'golf-pwa-v52';
 const APP_SHELL = [
   './',
   './index.html',
@@ -10,7 +10,6 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(APP_SHELL))
-      .then(() => self.skipWaiting())
   );
 });
 
